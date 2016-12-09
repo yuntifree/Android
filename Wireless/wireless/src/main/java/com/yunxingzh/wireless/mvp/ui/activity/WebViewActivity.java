bec0d5fc5a393924ac3ff2b2f403d019ad35267e
LiuShengDong <463159846@qq.com>
2016-12-09 16:34:51 +0800

    界面优化

--- Wireless/wireless/src/main/java/com/yunxingzh/wireless/mvp/ui/activity/WebViewActivity.java
+++ Wireless/wireless/src/main/java/com/yunxingzh/wireless/mvp/ui/activity/WebViewActivity.java
@@ -86,13 +86,13 @@ public class WebViewActivity extends BaseActivity implements View.OnClickListene
             @Override
             public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
                 super.onReceivedHttpError(view, request, errorResponse);
-                ToastUtil.showMiddle(WebViewActivity.this,R.string.internet_error);
+               // ToastUtil.showMiddle(WebViewActivity.this,R.string.internet_error);
             }
 
             @Override
             public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
                 super.onReceivedError(view, request, error);
-                ToastUtil.showMiddle(WebViewActivity.this,R.string.internet_error);
+               // ToastUtil.showMiddle(WebViewActivity.this,R.string.internet_error);
             }
         });
         myWebView.loadUrl(mUrl);
