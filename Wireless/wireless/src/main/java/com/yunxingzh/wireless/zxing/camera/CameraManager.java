2d36cd28e49a523514d6e0c643ca4d0e79b8a02e
LiuShengDong <463159846@qq.com>
2016-12-09 17:22:04 +0800

    扫码框比例放大

--- Wireless/wireless/src/main/java/com/yunxingzh/wireless/zxing/camera/CameraManager.java
+++ Wireless/wireless/src/main/java/com/yunxingzh/wireless/zxing/camera/CameraManager.java
@@ -38,9 +38,9 @@ public final class CameraManager {
 
   private static final String TAG = CameraManager.class.getSimpleName();
 
-  private static final int MIN_FRAME_WIDTH = 240;
+  private static final int MIN_FRAME_WIDTH = 480;
   private static final int MIN_FRAME_HEIGHT = 240;
-  private static final int MAX_FRAME_WIDTH = 480;
+  private static final int MAX_FRAME_WIDTH = 960;
   private static final int MAX_FRAME_HEIGHT = 360;
 
   private static CameraManager cameraManager;
