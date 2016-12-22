c64113ba05f0ad2ca0d5879153174f9546b62aed
LiuShengDong <463159846@qq.com>
2016-12-22 17:07:20 +0800

    合并

--- Wireless/wireless/src/main/java/com/yunxingzh/wireless/mview/PublicDialog.java
+++ Wireless/wireless/src/main/java/com/yunxingzh/wireless/mview/PublicDialog.java
@@ -1,7 +1,10 @@
 package com.yunxingzh.wireless.mview;
 
-import android.content.Context;
 import android.support.v4.app.DialogFragment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by stephen on 2016/12/22.
@@ -10,6 +13,11 @@ import android.support.v4.app.DialogFragment;
 
 public class PublicDialog extends DialogFragment {
 
-    private Context context;
+    private TextView mTitleTv;
+    private TextView mMessageTv;
+    private Button mPositiveBtn;//确定
+    private Button mNegativeBtn;//取消
+    private View mCloseIv;
+    private LinearLayout mContainerLay;
 
 }
